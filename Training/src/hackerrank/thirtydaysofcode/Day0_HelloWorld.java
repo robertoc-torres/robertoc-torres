3a59c01ce34a34b58b1dad90bc9deacb723b7abb
robertoc-torres <robertoc.torreslopez@gmail.com>
2021-07-18 07:01:14 -0400

    Changed name format for #30daysofcode challenge

--- Training/src/hackerrank/thirtydaysofcode/Day0_HelloWorld.java
+++ Training/src/hackerrank/thirtydaysofcode/Day0_HelloWorld.java
@@ -1,9 +1,22 @@
 package hackerrank.thirtydaysofcode;
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+
+/*
+
+    src:https://www.hackerrank.com/challenges/30-hello-world/problem?isFullScreen=true
+
+    Day 0 - HelloWorld
+
+    Objective
+    In this challenge, we review some basic concepts that will get you started with this series. You will need to use the same (or similar) syntax to read input and write output in challenges throughout HackerRank. Check out the Tutorial tab for learning materials and an instructional video!
+
+    Task
+    To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.
+
+    You've got this!
+
+ */
+
 public class Day0_HelloWorld {
     public static void main(String[] args) {
         // Create a Scanner object to read input from stdin.
